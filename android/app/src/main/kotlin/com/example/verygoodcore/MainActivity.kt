3457880cdc97ad01cb2f76927159c61211cd765d
omarsherifAdwat <omar.sherifs@live.com>
2023-12-17 22:57:02 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.client_app_focal_agent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:client_app_focal_agent/app.dart';
+import 'package:client_app_focal_agent/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
